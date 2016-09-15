d2a3ebd7acefafc8af3bee8c1b656e68dd8eca65
Jorge Miranda <https://github.com/jorgemirandaneto>
2016-09-15 16:26:42 -0300

    Criação de um nova lib

---
+++ src/main/java/br/pro/delfino/drogaria/util/JLibConfig.java
@@ -0,0 +1,12 @@
+package br.pro.delfino.drogaria.util;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("rest")
+public class JLibConfig extends ResourceConfig {
+	public JLibConfig(){
+		packages("br.pro.delfino.drogaria.service");
+	}
+}
